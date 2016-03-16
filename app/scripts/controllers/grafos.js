a3c2e189b353b7fc5809c2dcb2788089eb624413
Fer <ferbb123@gmail.com>
2016-03-16 13:44:58 +0100

    16/3/16
    habilitada opción de marcar/desmarcar grupos de neuronas

--- app/scripts/controllers/grafos.js
+++ app/scripts/controllers/grafos.js
@@ -12,6 +12,7 @@ angular
 
   .controller ('GrafosCtrl', ['$scope','grafoFactory', function($scope, grafoFactory){
 
+    var datosArray;
 
     $scope.mostrarNeuronasLeidas = function(arrayNeuronal) {
 
@@ -203,7 +204,6 @@ angular
     //Entrada: fichero de texto con datos neuronales
     //Salida: array bidimensional con las aristas para cada neurona
     $scope.cargarNeuronas = function($fileContent) {
-
       var lineas = $fileContent.split("\n");
       var palabrasPorLineas;
       var origen = [];
@@ -234,7 +234,15 @@ angular
         console.log(m+'mi array: '+items[m]);
       }
 
+      datosArray = items;
+
       $scope.mostrarNeuronasLeidas(items);
+
+      //Ocultamos los botones de opciones y activamos la selección de neuronas
+      document.getElementById("botones_opciones").style.display = 'none';
+      document.getElementById("seleccion_neuronas").style.display = 'block';
+
+
 
     }
 
@@ -318,12 +326,216 @@ angular
         //Mostramos el grafo por pantalla
         grafoFactory.cargar(jsonObj);
 
+
     }
 
     //Función para mostrar un grafo por defecto
     $scope.mostrar_grafo = function() {
         refresh();
         grafoFactory.crearGrafo();
+    }
+
+    //FUNCIÓN DE PRUEBA PARA MOSTRAR MENSAJES -------------------------
+    $scope.mostrarSaludo = function(numero) {
+      var x = document.getElementById(numero);
+      if (x.checked)
+        console.log ('La neurona número '+numero+ ' está activada');
+      else {
+        console.log ('La neurona número '+numero+' está desactivada');
+      }
+    }
+
+    $scope.mostrarDatos = function(check) {
+      var mosey = document.getElementById('1');
+      var granulle = document.getElementById('2');
+      var purkinje = document.getElementById('3');
+      var dcn = document.getElementById('4');
+      var golgi = document.getElementById('5');
+      var io = document.getElementById('6');
+
+      console.log ('tamaño datosArray: '+datosArray.length);
+      refresh();
+
+      //Creación del objeto json que alberga la información de nodos y aristas a dibujar
+      var jsonObj = { nodes: [{}], edges: [{}]};
+
+      //Proporciona valores por defecto a la variable nodo usada en el bucle
+      function getNodo() {
+        return {
+          id: "",
+          label: "",
+          x: "",
+          y: "",
+          size: ""
+        }
+      };
+
+      //Proporciona valores por defecto a la variable arista usada en el bucle
+      function getAristas() {
+        return {
+          id: "",
+          source: "",
+          target: ""
+        }
+      };
+
+      //Creación del lienzo inicial
+      for (var i=0; i<1871; i++) {
+        var nodo = getNodo();
+        var arista = getAristas();
+        nodo.id = "n"+i;
+        nodo.label = "neurona "+i;
+
+        //Mosey Fibers
+        if (i>=0 && i<248) {
+          var radius = 1;
+          var angle = Math.random()*Math.PI*2;
+          var x = Math.cos(angle)*radius;
+          var y = Math.sin(angle)*radius;
+          nodo.x=x;
+          nodo.y=y;
+          nodo.size = 1;
+
+          if (!mosey.checked)
+            nodo.hidden = true;
+
+          jsonObj.nodes[i] = nodo;
+
+          arista.id = "e"+i;
+          arista.source = "n" + i;
+          arista.target = "n" + i;
+          jsonObj.edges[i] = arista;
+
+          for (var j=1; j<datosArray[i].length;j++){
+            arista.id="e"+i;
+            arista.source="n"+i;
+            arista.target="n"+datosArray[i][j];
+            jsonObj.edges[i]=arista;
+          }
+
+        }
+
+        //Granulle Cells
+        else if (i>=248 && i<1748){
+          var radius = 4;
+          var angle = Math.random()*Math.PI*2;
+          var x = Math.cos(angle)*radius;
+          var y = Math.sin(angle)*radius;
+          nodo.x=x;
+          nodo.y=y;
+          nodo.size = 1;
+
+          if (!granulle.checked)
+            nodo.hidden = true;
+
+          jsonObj.nodes[i] = nodo;
+
+          arista.id = "e"+i;
+          arista.source = "n" + i;
+          arista.target = "n" + i;
+          jsonObj.edges[i] = arista;
+
+          for (var j=1; j<datosArray[i].length;j++){
+            arista.id="e"+i;
+            arista.source="n"+i;
+            arista.target="n"+datosArray[i][j];
+            jsonObj.edges[i]=arista;
+          }
+        }
+
+        //Purkinje Cells
+        else if(i>=1748 && i<1796) {
+          var radius = 6;
+          var angle = Math.random()*Math.PI*2;
+          var x = Math.cos(angle)*radius;
+          var y = Math.sin(angle)*radius;
+          nodo.x=x;
+          nodo.y=y;
+          nodo.size = 2;
+
+          if (!purkinje.checked)
+            nodo.hidden = true;
+
+          jsonObj.nodes[i] = nodo;
+
+          arista.id = "e"+i;
+          arista.source = "n" + i;
+          arista.target = "n" + i;
+          jsonObj.edges[i] = arista;
+
+          for (var j=1; j<datosArray[i].length;j++){
+            arista.id="e"+i;
+            arista.source="n"+i;
+            arista.target="n"+datosArray[i][j];
+            jsonObj.edges[i]=arista;
+          }
+        }
+
+        //DCN Cells
+        else if(i>=1796 && i<1820){
+          var radius = 8;
+          var angle = Math.random()*Math.PI*2;
+          var x = Math.cos(angle)*radius;
+          var y = Math.sin(angle)*radius;
+          nodo.x=x;
+          nodo.y=y;
+          nodo.size = 1;
+
+          if (!dcn.checked)
+            nodo.hidden = true;
+
+          jsonObj.nodes[i] = nodo;
+
+          arista.id = "e"+i;
+          arista.source = "n" + i;
+          arista.target = "n" + i;
+          jsonObj.edges[i] = arista;
+        }
+
+        //Golgi Cells
+        else if(i>=1820 && i<1823){
+          var radius = 10;
+          var angle = Math.random()*Math.PI*2;
+          var x = Math.cos(angle)*radius;
+          var y = Math.sin(angle)*radius;
+          nodo.x=x;
+          nodo.y=y;
+          nodo.size = 1;
+
+          if (!golgi.checked)
+            nodo.hidden = true;
+
+          jsonObj.nodes[i] = nodo;
+
+          arista.id = "e"+i;
+          arista.source = "n" + i;
+          arista.target = "n" + i;
+          jsonObj.edges[i] = arista;
+        }
+
+        //IO Cells
+        else if(i>=1823 && i<=1870){
+          var radius = 12;
+          var angle = Math.random()*Math.PI*2;
+          var x = Math.cos(angle)*radius;
+          var y = Math.sin(angle)*radius;
+          nodo.x=x;
+          nodo.y=y;
+          nodo.size = 1;
+
+          if (!io.checked)
+            nodo.hidden = true;
+
+          jsonObj.nodes[i] = nodo;
+
+          arista.id = "e"+i;
+          arista.source = "n" + i;
+          arista.target = "n" + i;
+          jsonObj.edges[i] = arista;
+        }
+
+      }
+      grafoFactory.cargar(jsonObj);
     }
 
   }])
@@ -353,11 +565,11 @@ angular
             container: document.getElementById('container2')
           },
           settings: {
-            defaultNodeColor: "#000000",
+            defaultNodeColor: "#ffffff",
             nodeHoverColor: "#0000ff",
             defaultNodeHoverColor: "#0000ff",
-            defaultEdgeColor: "#003300",
-            edgeColor: "#003300",
+            defaultEdgeColor: "#00ff00",
+            edgeColor: "#00ff00",
             defaultLabelHoverColor: "#0010ff",
             defaultLabelColor: '#0010ff'
           }
